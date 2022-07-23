931709df59d22cba99fb2c415e4b2905717647cf
git-jr <juniorobom01@gmail.com>
2022-07-22 23:38:19 -0300

    Fazendo busca ordenada direto na requisição

--- src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -1,17 +1,17 @@
 package br.com.alura.linguagens.api;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.ToIntFunction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,11 +23,7 @@ public class LinguagemController {
 
     @GetMapping("/linguagens")
     public List<Linguagem> obterLinguagens() {
-        List<Linguagem> linguagens = repositorio.findAll();
-
-        ToIntFunction<Linguagem> ordenarPorRanking = l -> l.getRanking();
-        Comparator<Linguagem> compararRanking = Comparator.comparingInt(ordenarPorRanking);
-        linguagens.sort(compararRanking);
+        List<Linguagem> linguagens = repositorio.findAll(Sort.by(Sort.Direction.ASC,"ranking"));
 
         return linguagens;
     }
@@ -38,23 +34,25 @@ public class LinguagemController {
         return new ResponseEntity<>(linguagemSalva, HttpStatus.CREATED);
     }
 
+    @PutMapping("/linguagens/{id}")
+    public ResponseEntity<Linguagem> atualizar(@RequestBody Linguagem linguagem, @PathVariable String id) {
+        Optional<Linguagem> linguagemAntiga = repositorio.findById(id);
+        linguagem.setId(linguagemAntiga.get().getId());
+        return new ResponseEntity<>(repositorio.save(linguagem), HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/linguagens/{id}")
+    public String deleta(@PathVariable String id) {
+        repositorio.deleteById(id);
+        return "Deletado";
+    }
+
     @GetMapping("/linguagens/{id}")
     public Linguagem buscaPorId(@PathVariable String id) {
         Optional<Linguagem> linguagem = repositorio.findById(id);
         return linguagem.get();
     }
 
-    @DeleteMapping("/linguagens/{id}")
-    public String deletaPorId(@PathVariable String id) {
-        repositorio.deleteById(id);
-        return "Deletado";
-    }
-
-    @PostMapping("/linguagens/{id}")
-    public ResponseEntity<Linguagem> update(@RequestBody Linguagem linguagem, @PathVariable String id) {
-        Optional<Linguagem> linguagemAntiga = repositorio.findById(id);
-        linguagem.setId(linguagemAntiga.get().getId());
-        return new ResponseEntity<>(repositorio.save(linguagem), HttpStatus.OK);
-    }
 
 }
