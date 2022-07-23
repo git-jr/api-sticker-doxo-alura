b3b114b679c2eaf50aff76439fe4237ca5e3aad3
git-jr <juniorobom01@gmail.com>
2022-07-22 23:33:23 -0300

    Fazendo busca ordenada direto na requisição

--- src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -1,9 +1,7 @@
 package br.com.alura.linguagens.api;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.ToIntFunction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
